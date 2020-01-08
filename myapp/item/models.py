898ee18213999780a7d66c5db7e08f3437f2fb5b
Yu Jeonghoon <moning02004@naver.com>
2020-01-08 12:45:57 +0900

    model design

--- myapp/item/models.py
+++ myapp/item/models.py
@@ -1,20 +1,35 @@
 from django.db import models
 
 
-class Item(models.Model):
-    name = models.CharField(max_length=32)
-    quantity = models.IntegerField()
+def image_path(instance, filename):
+    return 'image/{}'.format(filename)
 
 
-# class Item(models.Model):
-#     name = models.CharField(max_length=50)
-#     image_id = models.CharField(max_length=50)
-#     price = models.IntegerField()
-#     gender = models.CharField(max_length=6)
-#     category = models.CharField(max_length=15)
-#     ingredients = models.TextField(max_length=300)
-#     monthlySales = models.IntegerField()
-#
-#     def __str__(self):
-#         return self.name
-#
+def thumbnail_path(instance, filename):
+    return 'image/{}'.format(filename)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    gender = models.CharField(max_length=6)
+    category = models.CharField(max_length=15)
+    ingredients = models.TextField(max_length=300)
+    monthlySales = models.IntegerField()
+
+    thumbnail = models.ImageField(upload_to=thumbnail_path)
+    image = models.ImageField(upload_to=image_path)
+    image_id = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+    oily = models.CharField(max_length=1, default="")
+    dry = models.CharField(max_length=1, default="")
+    sensitive = models.CharField(max_length=1, default="")
+
+    def __str__(self):
+        return self.name
