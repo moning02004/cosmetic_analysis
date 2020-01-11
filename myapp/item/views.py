1480f86408ba921ff2510e14c3f62202a5faab33
Yu Jeonghoon <moning02004@naver.com>
2020-01-11 14:16:53 +0900

    fix products/~

--- myapp/item/views.py
+++ myapp/item/views.py
@@ -1,3 +1,5 @@
+import time
+
 from django.db.models import Q, Count, F
 from django.http import HttpResponse
 from rest_framework import status
@@ -15,39 +17,39 @@ class ProductsAPI(APIView):
     __base_url = "https://grepp-programmers-challenges.s3.ap-northeast-2.amazonaws.com/2020-birdview/thumbnail/"
 
     def get(self, request):
+        start = time.clock()
         skin_type = request.GET.get('skin_type')
         assert skin_type
+        # get params
+        include = request.GET.get('include_ingredient')
+        exclude = request.GET.get('exclude_ingredient')
+        category = request.GET.get('category')
 
-        include = [Ingredient.objects.get(name=x) for x in request.GET.get('include_ingredient').split(',')] \
-            if request.GET.get('include_ingredient') else []
-        exclude = [Ingredient.objects.get(name=x) for x in request.GET.get('exclude_ingredient').split(',')] \
-            if request.GET.get('exclude_ingredient') else []
-        category = Category.objects.get(name=request.GET.get('category')) \
-            if request.GET.get('category') else None
+        include = [Ingredient.objects.get(name=x) for x in include.split(',')] if include else []
+        exclude = [Ingredient.objects.get(name=x) for x in exclude.split(',')] if exclude else []
+
+        products = Category.objects.get(name=category).product_set.all() if category else Product.objects.all()
 
         extract_prod = []
-        products = Product.objects.all()
-        if category:
-            products = products.filter(category=category)
-
         for product in products:
             ingreds = product.ingredient.all()
             if set(ingreds) - set(exclude) == set(ingreds) and not len(set(include) - set(ingreds)):
-                score = len(product.ingredient.filter(**{skin_type: "O"})) - len(product.ingredient.filter(**{skin_type: "X"}))
-                extract_prod.append([score, product])
+                score = len(ingreds.filter(**{skin_type: "O"})) - len(ingreds.filter(**{skin_type: "X"}))
+                extract_prod.append([score, product, ingreds])
 
         extract_prod = sorted(extract_prod, key=lambda x: (-x[0], x[1].price))
         response = list()
-        for _, product in extract_prod:
+        for _, product, ingred in extract_prod:
             response.append({
-                'score': _,
                 'id': product.pk,
                 'imgUrl': self.__base_url + product.image_id + '.jpg',
                 'name': product.name,
                 'price': product.price,
-                'ingredients': ','.join([x.name for x in product.ingredient.all()]),
+                'ingredients': ','.join([x.name for x in ingred]),
                 'monthlySales': product.monthlySales
             })
+        print(time.clock() - start)
+        print(len(response))
         return Response(response, status=status.HTTP_200_OK)
 
 
